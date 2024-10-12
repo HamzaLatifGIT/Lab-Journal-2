5b7fe56353c3619741828dd8ea6ee1a37452f0e9
HamzaLatifGIT <hamzalatif006@gmail.com>
2024-10-12 14:05:52 +0500

    student-registration form

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -17,7 +17,7 @@ describe('AppComponent', () => {
   it(`should have the 'student-management' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('student-management');
+
   });
 
   it('should render title', () => {
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,84 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms'; 
+import { CommonModule } from '@angular/common'; 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule,CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'student-management';
+
+  student = {
+    name: '',
+    age: null,
+    email: '',
+    course: ''
+  };
+
+
+  onSubmit(form: any) {
+
+    if (form.valid) {
+      console.log('Form Submitted:', this.student);
+      alert('Registration Successful!');
+
+      form.reset();
+    } 
+    else {
+
+      let errorMessages = this.getErrorMessages(form);
+      alert('Please correct the following errors:\n' + errorMessages.join('\n'));
+    }
+  }
+
+  getErrorMessages(form: any): string[] {
+    const messages = [];
+
+
+    if (form.controls.name?.errors) {
+      if (form.controls.name.errors.required) {
+        messages.push('Name is required.');
+      }
+      if (form.controls.name.errors.minlength) {
+        messages.push('Name must be at least 3 characters long.');
+      }
+    }
+
+
+    if (form.controls.age?.errors) {
+      if (form.controls.age.errors.required) {
+        messages.push('Age is required.');
+      }
+      if (form.controls.age.errors.min) {
+        messages.push('Age must be at least 18.');
+      }
+      if (form.controls.age.errors.max) {
+        messages.push('Age must be less than or equal to 60.');
+      }
+    }
+
+
+    if (form.controls.email?.errors) {
+      if (form.controls.email.errors.required) {
+        messages.push('Email is required.');
+      }
+      if (form.controls.email.errors.email) {
+        messages.push('Invalid email format.');
+      }
+    }
+
+
+
+    if (form.controls.course?.errors) {
+      if (form.controls.course.errors.required) {
+        messages.push('Course is required.');
+      }
+    }
+
+    return messages;
+  }
 }
